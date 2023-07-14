777d8a910038f83b38510018373c31eef027afc6
sujithdusa-stride <sujith.kumar@stride.ai>
2023-07-14 14:30:03 +0530

    added integrations accesss

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 from environs import Env
+import json
 
 env = Env()
 env.read_env()
@@ -10,3 +11,10 @@ TRANSFORMER_CHANNEL=env.str("TRANSFORMER_CHANNEL")
 AGGREGATOR_CHANNEL=env.str("AGGREGATOR_CHANNEL")
 
 DYNAMODB_KEY=env.str("DYNAMODB_KEY")
+
+
+def get_integrations(feed_type: str):
+    integrations_file_path = 'config/integrations.json'
+    with open(integrations_file_path, 'r') as file:
+        data = json.load(file)
+    return data[feed_type]
